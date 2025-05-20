7f5ce942f9f36517b6cf1cb341ed021fe46e4d4a
TheInstinc <juliustegar123456@gmail.com>
2025-05-20 18:14:30 +0700

    Alpha commit

---
+++ doraemon.h
@@ -0,0 +1,34 @@
+#ifndef DORAEMON_H
+#define DORAEMON_H
+
+#include <GL/glut.h>
+#include <stdio.h>
+
+// Function declarations for Doraemon
+void initDoraemon();
+void drawDoraemon();
+void updateDoraemon();
+void setKeyState(unsigned char key, bool state);
+void setCameraMode(int mode);
+int getCameraMode();
+
+// Camera control functions
+void specialKeyPressed(int key, int x, int y);
+float getCameraX();
+float getCameraY();
+float getCameraZ();
+float getLookX();
+float getLookY();
+float getLookZ();
+
+// UI & Environment functions
+void drawText(const char* text, float x, float y, float r, float g, float b);
+void displayControlInfo();
+void drawGround();
+
+// Position getters for Doraemon
+float getDoraemonX();
+float getDoraemonY();
+float getDoraemonZ();
+
+#endif // DORAEMON_H
